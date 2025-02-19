a4a787d4944bed1541b57261425257c24805270d
Vinnie <The.CoolV1994@gmail.com>
2025-02-19 02:32:15 -0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -6,10 +6,12 @@ var urlParams = new URLSearchParams(queryString);
 var tagGawdTech = 'gawdtech-20';
 var tagDefault =  tagGawdTech;
 
-
-var idTextBox = 'url-old';
-var idHyperLink = 'url-new';
-
+var idLinkForm = 'link-old';
+var idTextBoxOld = 'url-old';
+var idLinkNew = 'link-new';
+var idTextBoxNew = 'url-new';
+var idRedirect = 'redirect';
+var idRedirectLink = 'redirect-url';
 
 var itemID = urlParams.get('item');
 console.log(`Iten ID: ${itemID}`);
@@ -22,18 +24,18 @@ console.log(`Tag ID: ${tagID}`);
 function getAmazonURL (itemID, tagID=tagDefault)
 {
   var amazonURL = "https://www.amazon.com";
-  var itemURL = "/dp/${itemID}";
-  var tagURL = "?tag=${tagID}";
-  var fullURL = amazonURL+itemURL+tagURL;
+  var itemURL = `/dp/${itemID}`;
+  var tagURL = `?tag=${tagID}`;
+  var fullURL = `${amazonURL}${itemURL}${tagURL}`;
   return fullURL;
 }
 
 function redirect (itemID, tagID=tagDefault)
 {
   var url = getAmazonURL (itemID, tagID);
-  window.location.href = url;
-  window.location.replace(url);
-  setLink (url);
+  window.location.href = `${url}`;
+  window.location.replace(`${url}`);
+  setLink (`${url}`);
 }
 
 function getItemID (url)
@@ -47,45 +49,47 @@ function getItemID (url)
 
 function setLink (url)
 {
-  var hyperlink = getHyperLink();
-  hyperlink.text = url;
-  hyperlink.src = url;
+  var hyperlink = getElement(idRedirectLink);
+  hyperlink.text = `${url}`;
+  hyperlink.src = `${url}`;
 }
 
-function getHyperLink (id=idHyperLink) {
-  var hyperlink = document.getElementById(id);
+function getElement (id) {
+  var element = document.getElementById(id);
+  return element;
 }
 
-function getTextBox (id=idTextBox) {
-  var textBox = document.getElementById(id);
-  return textBox;
-}
-
-function copyText()
+function copyText(id)
 {
-    var textBox = getTextBox();
+    var textBox = getElement(id);
     textBox.select();
     textBox.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(textBox.value);
     alert("Text Copied To Clipboard:\n" + textBox.value);
 }
 
-function clearText()
+function setText(id, text)
 {
-    var textBox = getTextBox();
-    textBox.value = "";
+    var element = getElement(id);
+    element.text = `${text}`;
 }
 
-function generateLink ()
+function setValue(id, text)
 {
-  var textBox = getTextBox();
+    var textBox = getElement(id);
+    textBox.value = `${text}`;
+}
+
+function generateLink (idOld, idNew)
+{
+  var textBox = getElement(idOld);
   var itemID = getItemID(textBox.value);
-  var link = getAmazonURL(itemID, tagGawdTech);
+  var link = getAmazonURL(itemID, tagDefault);
+  setValue(idNew, link);
 }
 
 
 if (itemID) {
   redirect(itemID. tagID);
-  return;
 }
 
