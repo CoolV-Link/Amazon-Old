247a87bf080bfffe0fe1b6e2ba9e14797815e30d
Vinnie <The.CoolV1994@gmail.com>
2025-02-19 02:10:57 -0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,63 +1,91 @@
-var queryString = window.location.search;
 
+var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 
+
 var tagGawdTech = 'gawdtech-20';
-
-var baseURL = 'https://www.amazon.com';
-
-var itemID = urlParams.get('item')
-console.log(itemID);
-
-var tagID = urlParams.get('tag')
-console.log(tagID);
-
-if (!tagID) {
-  tagID = tagGawdTech;
-}
-
-var itemURL = "/dp/${itemID}";
-
-var tagURL = "?tag=${tagID}";
-
-var linkURL = "baseURL+itemURL+tagURL;
+var tagDefault =  tagGawdTech;
 
 
-if (itemID) {
-  redirect("${linkURL}");
-  return;
-}
+var idTextBox = 'url-old';
+var idHyperLink = 'url-new';
 
 
-function redirect (url)
+var itemID = urlParams.get('item');
+console.log(`Iten ID: ${itemID}`);
+
+var tagID = urlParams.get('tag');
+console.log(`Tag ID: ${tagID}`);
+
+
+
+function getAmazonURL (itemID, tagID=tagDefault)
 {
+  var amazonURL = "https://www.amazon.com";
+  var itemURL = "/dp/${itemID}";
+  var tagURL = "?tag=${tagID}";
+  var fullURL = amazonURL+itemURL+tagURL;
+  return fullURL;
+}
+
+function redirect (itemID, tagID=tagDefault)
+{
+  var url = getAmazonURL (itemID, tagID);
   window.location.href = url;
   window.location.replace(url);
+  setLink (url);
 }
 
-function setLink (id, url)
+function getItemID (url)
 {
-  var textURL = document.getElementById(id);
-  textURL.text = url;
-  textURL.src = url;
+  var itemStart = paragraph.indexOf('/dp/');
+  itemStart += 4;
+  var itemEnd = paragraph.indexOf('/', itemStart);
+  var itemID = text.substring(itemStart, itemEnd);
+  return itemID;
 }
 
-function copyText(id)
+function setLink (url)
 {
-    var textBox = document.getElementById(id);
+  var hyperlink = getHyperLink();
+  hyperlink.text = url;
+  hyperlink.src = url;
+}
+
+function getHyperLink (id=idHyperLink) {
+  var hyperlink = document.getElementById(id);
+}
+
+function getTextBox (id=idTextBox) {
+  var textBox = document.getElementById(id);
+  return textBox;
+}
+
+function copyText()
+{
+    var textBox = getTextBox();
     textBox.select();
     textBox.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(textBox.value);
     alert("Text Copied To Clipboard:\n" + textBox.value);
 }
 
-function clearText(id)
+function clearText()
 {
-    var textBox = document.getElementById(id);
+    var textBox = getTextBox();
     textBox.value = "";
 }
 
-function newLink ()
+function generateLink ()
 {
-  
+  var textBox = getTextBox();
+  var itemID = getItemID(textBox.value);
+  var link = getAmazonURL(itemID, tagGawdTech);
 }
+
+
+if (itemID) {
+  redirect(itemID. tagID);
+  return;
+}
+
