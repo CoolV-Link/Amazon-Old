168b1c6c900bb977e1ca602f7f953cd1a0363096
Vinnie <The.CoolV1994@gmail.com>
2025-02-21 18:38:38 -0500

    Update redirect.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,1 +1,25 @@
+/** Amazon Redirect **/
 
+function redirect (url)
+{
+  window.location.replace(`${url}`);
+  //window.location.href = `${url}`;
+}
+
+function getRedirectURL ()
+{
+  if (ARG_LIST) {
+    return `${URL_SITE}/List/?list=${ARG_LIST}`;
+  }
+  if (ARG_ITEM) {
+    return getAmazonURL(ARG_ITEM, ARG_TAG);
+  }
+  return `${URL_SITE}/Link/`;
+}
+
+function pageRedirect (id)
+{
+  var url = getRedirectURL();
+  setLink(id, url);
+  redirect(url);
+}
