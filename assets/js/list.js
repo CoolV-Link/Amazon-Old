bd73f3bdbb84ded062184d55bfa074d20e857d55
Vinnie <The.CoolV1994@gmail.com>
2025-02-21 18:46:45 -0500

    Update list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,1 +1,46 @@
+/** Amazon List **/
 
+/**
+// Sample Lists
+var test = new List("", "List", "Sample");
+test.items = [
+  new Item("", "Item", "Sample", 0.00),
+];
+LISTS.push(test);
+**/
+
+var LISTS = [];
+
+function List (id, name, info)
+{
+  this.id = id;
+  this.name = name;
+  this.info = info;
+  this.items = [];
+}
+
+function Item (id, name, info, price)
+{
+  this.id = id;
+  this.name = name;
+  this.info = info;
+  this.price = price;
+}
+
+function showList (id)
+{
+  for (var list : LISTS)
+    {
+        if (id && id != list.id)
+        {
+          continue;
+        }
+        print(list);
+    }
+}
+
+function print (list)
+{
+  console.log(`[${list.id}] ${list.name}`);
+  console.log(list.items);
+}
