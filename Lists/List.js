1136413ffea909901a980105f219d0ca50b0ae4a
Vinnie <The.CoolV1994@gmail.com>
2025-02-20 19:36:09 -0500

    Update List.js

--- Lists/List.js
+++ Lists/List.js
@@ -1,16 +1,16 @@
-var list = '';
+var list = new List(0, "List", "Sample");
 var items = [
-  
+  new Item(0, "Item", 0.00, "Sample"),
 ];
 
-function List (id, name, info='')
+function List (id, name, info)
 {
   this.id = id;
   this.name = name;
   this.info = info;
 }
 
-function Item (id, name, price, info='')
+function Item (id, name, price, info)
 {
   this.id = id;
   this.name = name;
