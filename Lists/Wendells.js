0bb69dc19c27ea1b96545f2d7c2476224bd090d3
Vinnie <The.CoolV1994@gmail.com>
2025-02-20 20:21:34 -0500

    Update Wendells.js

--- Lists/Wendells.js
+++ Lists/Wendells.js
@@ -3,7 +3,7 @@
 // List: History
 var history = new List("TPYL1KJFQ5X9", "History", "Order History");
 history.items = [
-  new Item("B07FZ3G7SC", "Bluetooth Adapter", 23.99, "esinkin Wireless Audio Receiver for Music Streaming Sound System Works with Smart Phones, Tablets and Car, Wireless Adapter for Speakers"),
+  new Item("B07FZ3G7SC", "Bluetooth Adapter", "esinkin Wireless Audio Receiver for Music Streaming Sound System Works with Smart Phones, Tablets and Car, Wireless Adapter for Speakers", 23.99),
 ];
 LISTS.push(history);
 
@@ -11,7 +11,7 @@ LISTS.push(history);
 // List: Cart
 var cart = new List("3BX6C2Q2CY8Z4", "Cart", "Shopping Cart");
 cart.items = [
-  new Item("", "Item", 0.00, "Sample"),
+  new Item("", "Item", "Sample", 0.00),
 ];
 LISTS.push(cart);
 
@@ -19,7 +19,7 @@ LISTS.push(cart);
 // List: Wish List
 var wishlist = new List("22BGSVXAYLNR9", "Saved", "Wish List");
 wishlist.items = [
-  new Item("", "Item", 0.00, "Sample"),
+  new Item("", "Item", "Sample", 0.00),
 ];
 LISTS.push(wishlist);
 
