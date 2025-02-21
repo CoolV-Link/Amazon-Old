b092711eebe1aadff6e6bb0c4375d0b15f8efe71
Vinnie <The.CoolV1994@gmail.com>
2025-02-20 19:57:31 -0500

    Create Wendells.js

---
+++ Lists/Wendells.js
@@ -0,0 +1,25 @@
+// Wendell's Pub Lists
+
+// List: History
+var history = new List("TPYL1KJFQ5X9", "History", "Order History");
+history.items = [
+  new Item("", "Item", 0.00, "Sample"),
+];
+LISTS.push(history);
+
+
+// List: Cart
+var cart = new List("3BX6C2Q2CY8Z4", "Cart", "Shopping Cart");
+cart.items = [
+  new Item("", "Item", 0.00, "Sample"),
+];
+LISTS.push(cart);
+
+
+// List: Wish List
+var wishlist = new List("", "Saved", "Wish List");
+wishlist.items = [
+  new Item("", "Item", 0.00, "Sample"),
+];
+LISTS.push(wishlist);
+
