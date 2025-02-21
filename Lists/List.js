13c4b267adb71710197d899b8a38f5ded19f6683
Vinnie <The.CoolV1994@gmail.com>
2025-02-20 19:43:54 -0500

    Update List.js

--- Lists/List.js
+++ Lists/List.js
@@ -1,13 +1,19 @@
-var list = new List(0, "List", "Sample");
-var items = [
+var LISTS = [];
+
+/** Sample Lists
+var test = new List(0, "List", "Sample");
+test.items = [
   new Item(0, "Item", 0.00, "Sample"),
 ];
+LISTS.push(test);
+**/
 
 function List (id, name, info)
 {
   this.id = id;
   this.name = name;
   this.info = info;
+  this.items = [];
 }
 
 function Item (id, name, price, info)
