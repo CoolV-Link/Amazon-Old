7c82a9d787234d7b4a3cd960211fd3981547ee11
Vinnie <The.CoolV1994@gmail.com>
2025-02-22 02:30:47 -0500

    Delete Lists.js

--- Lists.js
+++
@@ -1,6 +0,0 @@
-// List: History
-var history = new List("TPYL1KJFQ5X9", "History", "Order History");
-history.items = [
-  new Item("B07FZ3G7SC", "Bluetooth Adapter", "esinkin Wireless Audio Receiver for Music Streaming Sound System Works with Smart Phones, Tablets and Car, Wireless Adapter for Speakers", 23.99),
-];
-LISTS.push(history);
