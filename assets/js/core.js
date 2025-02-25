c4b68e965651077bc6f716f76a67908769269468
Vinnie <The.CoolV1994@gmail.com>
2025-02-24 22:44:34 -0500

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -49,7 +49,7 @@ function getStartItemID (url)
   URL_ITEM_PREFIX.forEach(
   (prefix) => {
     var index = url.indexOf(prefix);
-    if (index != -1) {
+    if (index) {
       return index + prefix.length;
     }
   });
