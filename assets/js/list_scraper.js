b4879251195cfe6924280c51636ed0d1afed2982
Vinnie <The.CoolV1994@gmail.com>
2025-02-24 20:36:10 -0500

    Update list_scraper.js

--- assets/js/list_scraper.js
+++ assets/js/list_scraper.js
@@ -1,5 +1,38 @@
 /** Amazon List Scraper **/
 
+function getListHTML (id)
+{
+}
 
-var listElement = getElement('awl-list-items');
 
+function getListInfo (id, doc)
+{
+  var title = listItem.querySelector('.awl-item-title');
+  var info = listItem.querySelector('.awl-item-title');
+  var owner = listItem.querySelector('.awl-item-title');
+  var listParent = doc.getElementById('awl-list-items');
+
+  var list = new List(id, title, info);
+  list.items = getListItems(listParent);
+  return list;
+}
+
+
+function getListItems (listParent)
+{
+  var items = [];
+  var listItems = listParent.querySelector('li');
+  for (var i = 0; i < listItems.length; i++) {
+    items.push(createListItem(listItems[i]));
+  }
+  return items;
+}
+
+function createItem (listItem)
+{
+  var id = listItem.querySelector('.awl-item-title');
+  var title = listItem.querySelector('.awl-item-title');
+  var info = listItem.querySelector('.awl-item-title');
+  var price = listItem.querySelector('.awl-item-title');
+  return new ListItem(id, title, info, price);
+}
