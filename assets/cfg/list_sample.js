760fa9b78a00c3c01bd96419b7fde8eaf9c3068b
Vinnie <The.CoolV1994@gmail.com>
2025-02-24 19:45:04 -0500

    Create list_sample.js

---
+++ assets/cfg/list_sample.js
@@ -0,0 +1,10 @@
+/** List Config **/
+
+// List: Test
+var test = new List("test", "Test List", "Sample Info");
+test.items = [
+  new Item("1", "Item 1", "Sample Info 1", 1.00),
+  new Item("2", "Item 2", "Sample Info 2", 2.00),
+  new Item("3", "Item 3", "Sample Info 3", 3.00),
+];
+LISTS.push(test);
