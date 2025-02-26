8e6dbfc68b5f7fd9ed103a7e951c657272ebe00b
Vinnie <The.CoolV1994@gmail.com>
2025-02-26 15:49:04 -0500

    Update config.js

--- assets/cfg/config.js
+++ assets/cfg/config.js
@@ -2,18 +2,24 @@
 
 var URL_SITE = "https://a.gt.myz.info";
 
+var URL_ITEM = `${URL_SITE}/`;
 var URL_LINK = `${URL_SITE}/Link/`;
 var URL_LIST = `${URL_SITE}/List/`;
-var URL_ITEM = `${URL_SITE}/`:
+var URL_CFG_LIST = "/assets/cfg/list/";
 
 var URL_ARG_ITEM = "item";
-var URL_ARG_LIST = "list";
 var URL_ARG_TAG = "tag";
+var URL_ARG_LIST = "list";
+var URL_ARG_LIST_CFG = "cfg";
+
+//var TAG_DEFAULT = TAGS[0];
+
+var TAG_DEFAULT = "gawdtech-20";
+
+var CFG_LIST_DEFAULT = "default";
+
 
 var TAGS = [
   "gawdtech-20",
 ];
 
-//var TAG_DEFAULT = TAGS[0];
-
-var TAG_DEFAULT = "gawdtech-20";
