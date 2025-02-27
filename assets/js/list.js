ce662d08d99fa0d55bb280daff49e2e7e6f0f74e
Vinnie <The.CoolV1994@gmail.com>
2025-02-27 09:53:18 -0500

    Update list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,6 +1,6 @@
 /** Amazon List **/
 
-var LISTS = [];
+//var LISTS = [];
 
 function List (id, name, info)
 {
@@ -18,89 +18,114 @@ function Item (id, name, info, price)
   this.price = price;
 }
 
-function pageList(idName, idList)
+
+function pageList(idPage, nodeTitle)
 {
-  //var cfgURL = getListConfigURL(cfgList);
-  //if (!ARG_LIST_CFG) {
-    //newAmazonList(ARG_LIST);
-  //}
-  //LoadListConfig(cfgURL, cfgInit),
-  if (!ARG_LIST) {
-    logList("-", "Lists", LISTS);
-    setListTitle(idName, "Lists");
-    setListItems(idList, LISTS);
+  var nodePage = getElement(idPage);
+  var nodeTitle = getElement(nodeTitle);
+
+  if (!ARG_LIST_CFG) {
+    nodeTitle.innerHTML = "Error: Invalid File";
     return;
   }
-  LISTS.forEach(
+
+  nodeTitle.innerHTML = "List: "+ARG_LIST_CFG;
+  var lists = loadSavedList(ARG_LIST_CFG);
+
+  if (!ARG_LIST) {
+    logList("Lists: ", lists);
+    newListItems(lists);
+    return;
+  }
+
+  lists.forEach(
     (list) => {
-//      if (list.id == ARG_LIST) {
-        logList(list.id, list.name, list.items);
-        setListTitle(idName, list.name);
-        setListItems(idList, list.items);
-//      }
+        logList(`[${list.id}] ${list.name}: `, list.items);
+        var listContainer = newListContainer(list);
+        nodePage.appendChild(listContainer);
     });
 }
 
-function setListTitle (idName, name)
+function newListItems (items)
 {
-  var title = getElement(idName);
-  title.innerHTML = name;
-}
-
-function setListItems (idList, items)
-{
-  var ul = getElement(idList);
+  var ul = document.createElement('ul');
   ul.innerHTML = "";
   items.forEach(
     (item) => {
-      ul.appendChild(newListItem(item));
+      var li = newTextLink('li', item.name, '#'+item.id);
+      ul.appendChild(li);
     });
+  return ul;
 }
 
-function newListItem (item)
+function newTextNode (type, text)
 {
-  var listItem = document.createElement('li');
-  var itemLink = document.createElement('a');
-  var itemText = createTextNode(item.name);
-  itemLink.href = item.id;
-  itemLink.appendChild(itemText);
-  listItem.appendChild(itemLink);
-  return listItem;
-}
-
-
-function logList (id, name, items)
-{
-  console.log(`[${id}] ${name}`);
-  console.log(items);
-}
-
-var jsInitAmazon = function ()
-{
-  downloadAmazonList(ARG_LIST),
-}
-
-var jsInitConfig = function ()
-{
-  loadList(ARG_LIST);
-}
-
-function loadListConfig (url, func)
-{
-  //cfgList = cfgList ? cfgList: CFG_LIST_DEFAULT;
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = url;
-  script.onload = func;
-  script.onreadystatechange = func;
-  document.body.appendChild(script);
-}
-
-function getListConfigURL (fileName)
-{
-  if (!fileName) {
-    fileName = LIST_DEFAULT;
-    //return `${URL_SITE}/assets/js/list_amazon.js`;
+  var textNode = createTextNode(text);
+  if (!type) {
+    return textNode;
   }
-  return `${URL_SITE}/assets/cfg/list/${fileName}.js`;
+  var container = document.createElement(type);
+  container.appendChild(textNode);
+  return container;
+}
+
+function newTextLink (type, text, url)
+{
+  var textNode = createTextNode(text);
+  var a = document.createElement('a');
+  a.href = url;
+  a.appendChild(textNode);
+  if (!type) {
+    return a;
+  }
+  var container = document.createElement(type);
+  container.appendChild(a);
+  return container;
+}
+
+function newListContainer (list)
+{
+  var container = document.createElement('div');
+  container.id = 'list-'+list.id;
+  var lineBreak = document.createElement('br');
+
+  container.appendChild(newTextLink('h1', list.name, '#'+list.id));
+  container.appendChild(newContainerNode('p', 'br',
+      createTextNode(null, list.info),
+      newTextLink(null, 'Owner: '+list.owner.name, '#'+list.owner.id)));
+  //container.appendChild(newParagraph(list.info, 'Owner: '+list.owner));
+  container.appendChild(newListItems(list.items));
+
+  return container;
+}
+
+function newContainerNode (type, spacer, ...nodes)
+{
+  var container = document.createElement(type);
+  var separator = document.createElement(spacer);
+  nodes.forEach(
+    (node) => {
+      if (index > 0) {
+        container.appendChild(separator);
+      }
+      container.appendChild(node);
+      index++;
+    });
+  return container;
+}
+
+function loadSavedList(listID)
+{
+  var url = `${URL_SITE}/assets/list/${listID}.json`;
+  var data = (await (await fetch(url)).text());
+  var json = JSON.parse(data);
+  return json;
+}
+
+function logList (text, ...args)
+{
+  args.forEach(
+    (arg) => {
+      console.log(text+args);
+    });
 }
