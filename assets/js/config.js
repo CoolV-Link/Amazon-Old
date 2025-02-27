477da70c2da484931e6f46ba8c6e52dc3db1b22a
Vinnie <The.CoolV1994@gmail.com>
2025-02-27 02:41:15 -0500

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,17 @@
+/** Amazon Config **/
+
+// Website URL
+var URL_SITE = "https://a.gt.myz.info";
+
+// URL for Saved Lists Dir
+var URL_SAVED_LISTS = `${URL_SITE}/assets/cfg/lists/`;
+
+// Default Tag to use for Affiliate Links
+var TAG_DEFAULT = "gawdtech-20";
+
+// List of Selectable Tags
+var TAGS = new Map();
+
+// TAGS.put("__TAG_ID__", "__TAG_PARENT__");
+TAGS.put("gawdtech-20", "gawdtech-20");
+TAGS.put("thecoolv-20", "gawdtech-20");
